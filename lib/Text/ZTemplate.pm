1ac15f6692c523026e32893e99ce34975b2df3c9
Toby Inkster <mail@tobyinkster.co.uk>
2026-03-05 23:57:08 +0000

    stuff

--- lib/Text/ZTemplate.pm
+++ lib/Text/ZTemplate.pm
@@ -458,4 +458,19 @@ Individual tags can override the default:
 
 L<Data::ZPath>, L<https://zpath.me/#ztemplate>
 
-=cut
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2026 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
